357852c41ca751603889cca66aad855b2ecc5e5b
REMELH01 <rmelhornmusic@gmail.com>
2025-03-23 10:49:33 -0400

    finished plots for now; need to add findings. updated requirements.txt file

--- MakingHitsPlots.py
+++ MakingHitsPlots.py
@@ -4,10 +4,10 @@ import numpy as np
 import mplcursors
 import plotly.express as px
 
+HitSongs = pd.read_csv('HitSongs.csv')
+
 # Here is out first plot. This is a scatter plot comparing the danceability and energy of the songs.
 # Please hover your mouse over the points on this plot to see what song is represented by that plot point! 
-
-HitSongs = pd.read_csv('HitSongs.csv')
 
 fig, ax = plt.subplots()
 scatter = ax.scatter(HitSongs['danceability'], HitSongs['energy'])
@@ -17,7 +17,7 @@ ax.set_xlabel('Danceability')
 ax.set_ylabel('Energy')
 plt.show()
 
-#Here is the second plot. My goal here is to examine the trends between danceability, energy, and valence, and see their correlation. 
+# Here is the second plot. My goal here is to examine the trends between danceability, energy, and valence, and see their correlation. 
 
 plt.plot(HitSongs['energy'], label ='Energy', color = 'red')
 plt.plot(HitSongs['valence'], label = 'Valence', color = 'blue')
@@ -29,4 +29,21 @@ plt.ylabel('Energy, Valence, and Danceability')
 plt.show()
 
 
-# 
+# Next, I would like to see box plots for all the columns I did calculations on.
+# As a reminder, these were dancebility, energy, speechiness, acousticness, valence, and tempo.
+
+HitSongs[['danceability', 'energy', 'speechiness', 'acousticness', 'valence']].boxplot()
+plt.xlabel('Song Features')
+plt.ylabel('Scale of 0.0 to 1.0')
+plt.title('Song Feature Boxplot')
+plt.grid(False)
+plt.show()
+
+
+# Interesting! 
+# I want to see what kind of histograms these 5 columns will look like and what they will tell us!
+
+HitSongs[['danceability', 'energy', 'speechiness', 'acousticness', 'valence']].hist(figsize = (10, 7))
+plt.show()
+
+# This is all for the plots. Please return to the MakingAHit.ipynb file to see my findings!
