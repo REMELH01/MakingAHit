8c8c1833080ffdb9049935163a60cdde2b6feab8
REMELH01 <rmelhornmusic@gmail.com>
2025-03-22 22:41:46 -0400

    added second plot to python file. need at least 1 more plot; likely will do 2 to 3 more for best data analysis

--- MakingHitsPlots.py
+++ MakingHitsPlots.py
@@ -12,12 +12,21 @@ HitSongs = pd.read_csv('HitSongs.csv')
 fig, ax = plt.subplots()
 scatter = ax.scatter(HitSongs['danceability'], HitSongs['energy'])
 mplcursors.cursor(scatter, hover = True).connect("add", lambda sel: sel.annotation.set_text(f"{HitSongs['song'][sel.index]} by {HitSongs['artists'].iloc[sel.index]}"))
-
 ax.set_title('Danceability and Energy Comparison')
 ax.set_xlabel('Danceability')
 ax.set_ylabel('Energy')
+plt.show()
 
+#Here is the second plot. My goal here is to examine the trends between danceability, energy, and valence, and see their correlation. 
+
+plt.plot(HitSongs['energy'], label ='Energy', color = 'red')
+plt.plot(HitSongs['valence'], label = 'Valence', color = 'blue')
+plt.plot(HitSongs['danceability'], label = 'Danceability', color = 'purple')
+plt.legend()
+plt.title('Examining the Trends of Energy, Valence, and Danceability')
+plt.xlabel('Number of songs')
+plt.ylabel('Energy, Valence, and Danceability')
 plt.show()
 
 
-
+# 
